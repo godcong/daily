805c1bfd4a051fb4151829d8415599a8c580e0da
godcong <jumbycc@163.com>
2018-12-04 08:16:34 +0800

    add command

--- command.go
+++ command.go
@@ -2,12 +2,14 @@ package daily
 
 import (
 	"fmt"
+	"os"
 
 	"os/exec"
 )
 
 func Command(name string) {
 	cmd := exec.Command("git", "version")
+	cmd.Env = os.Environ()
 	if whoami, err := cmd.Output(); err != nil {
 		fmt.Println(err)
 	} else {
